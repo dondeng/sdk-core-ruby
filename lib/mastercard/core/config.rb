4eabf511c4e78da313dc21df8021145173beb273
Naman Aggarwal <naman.aggarwal@yahoo.com>
2016-05-12 19:03:32 +0800

    Moved config to core

--- lib/mastercard/core/config.rb
+++ lib/mastercard/core/config.rb
@@ -2,62 +2,56 @@ require "mastercard/core/constants"
 
 module MasterCard
   module Core
-    module Config
-      class Config
-        include MasterCard::Core::Constants
+    class Config
 
-        private
-        @@sandbox = true
-        @@debug   = false
-        @@authentication = nil
-        @@localhost  = false
+      private
+      @@sandbox = true
+      @@debug   = false
+      @@authentication = nil
+      @@localhost  = false
 
-        public
-        def self.setDebug(debug)
-          @@debug = debug
+      public
+      def self.setDebug(debug)
+        @@debug = debug
+      end
+
+      def self.getDebug()
+        return @@debug
+      end
+
+      def self.setSandbox(sandbox)
+        @@sandbox = sandbox
+      end
+
+      def self.getSandbox()
+        return @@sandbox
+      end
+
+      def self.setLocal(local)
+        @@localhost = local
+      end
+
+      def self.getLocal
+        return @@localhost
+      end
+
+      def self.setAuthentication(auth)
+        @@authentication = auth
+      end
+
+      def self.getAuthentication
+        return @@authentication
+      end
+
+      def self.getAPIBaseURL
+
+        if @@localhost
+          return Constants::API_BASE_LOCALHOST_URL
+        elsif @@sandbox
+          return Constants::API_BASE_SANDBOX_URL
         end
 
-        def self.getDebug()
-          return @@debug
-        end
-
-        def self.setSandbox(sandbox)
-          @@sandbox = sandbox
-        end
-
-        def self.getSandbox()
-          return @@sandbox
-        end
-
-        def self.setLocal(local)
-          @@localhost = local
-        end
-
-        def self.getLocal
-          return @@localhost
-        end
-
-        def self.setAuthentication(auth)
-          @@authentication = auth
-        end
-
-        def self.getAuthentication
-          return @@authentication
-        end
-
-        def self.getAPIBaseURL
-
-          if @@localhost
-            return API_BASE_LOCALHOST_URL
-          elsif @@sandbox
-            return API_BASE_SANDBOX_URL
-          end
-
-          return API_BASE_LIVE_URL
-        end
-
-
-
+        return Constants::API_BASE_LIVE_URL
       end
     end
   end
--- test/test_config.rb
+++ test/test_config.rb
@@ -2,8 +2,7 @@ require 'minitest/autorun'
 require 'mastercard-core'
 
 class ConfigTest < Minitest::Test
-  include MasterCard::Core::Config
-  include MasterCard::Core::Constants
+  include MasterCard::Core
 
   def setup
 
@@ -14,7 +13,7 @@ class ConfigTest < Minitest::Test
   def test_config_get_url
 
     url = Config.getAPIBaseURL()
-    assert_equal(API_BASE_SANDBOX_URL,url)
+    assert_equal(Constants::API_BASE_SANDBOX_URL,url)
 
   end
 
@@ -22,7 +21,7 @@ class ConfigTest < Minitest::Test
 
     Config.setLocal(true)
     url = Config.getAPIBaseURL()
-    assert_equal(API_BASE_LOCALHOST_URL,url)
+    assert_equal(Constants::API_BASE_LOCALHOST_URL,url)
     assert(Config.getLocal())
   end
 
