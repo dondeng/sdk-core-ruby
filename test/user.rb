94e1f2ff4b3796edcf3130842abf35f511e84141
Andrea Rizzini <andrea_rizzini@mastercard.com>
2016-11-04 12:33:37 +0000

    arizzini: added tests for delete 204 and delete 200 with no content

--- test/user.rb
+++ test/user.rb
@@ -32,140 +32,227 @@ module MasterCard
     module Test
         class User < MasterCard::Core::Model::BaseObject
             include MasterCard::Core::Model
-            #
-
-            @__store = {
-                'bc2db01d-cf66-4050-a9af-4a6c67a5a7be' => OperationConfig.new("/mock_crud_server/users", "list", [], []),
-                'e53e61ff-ffa8-4d6f-9e2b-e85bfc70609a' => OperationConfig.new("/mock_crud_server/users", "create", [], []),
-                '989b48f8-87d2-4551-928e-441e3cc3a1ff' => OperationConfig.new("/mock_crud_server/users/{id}", "read", [], []),
-                'b39fbd58-cbf0-48ba-9971-e05101ec2309' => OperationConfig.new("/mock_crud_server/users/{id}", "update", [], []),
-                '54f33e70-0c93-472a-8369-b45c59ae11cb' => OperationConfig.new("/mock_crud_server/users/{id}", "delete", [], []),
-
-            }
-
-            protected
-
-            def self.getOperationConfig(uuid)
-                if @__store.key?(uuid)
-                    return @__store[uuid]
-                end
-                raise NotImplementedError.new("Invalid operationUUID supplied:"+ uuid)
-            end
-
-            def self.getOperationMetadata()
-                return OperationMetadata.new("0.0.1", "http://localhost:8081")
-            end
-
-            public
-
-
-
-
-
-
-            def self.listByCriteria(criteria = nil)
                 #
-                #List objects of type User
-                #
-                #@param Dict criteria
-                #@return Array of User object matching the criteria.
 
-                if criteria.nil?
-                    return self.execute("bc2db01d-cf66-4050-a9af-4a6c67a5a7be",User.new)
-                else
-                    return self.execute("bc2db01d-cf66-4050-a9af-4a6c67a5a7be",User.new(criteria))
-                end
-            end
+                @__store = {
+                    '6ac0b4ea-c45d-490b-96a5-840c4f8415e8' => OperationConfig.new("/mock_crud_server/users", "list", [], []),
+                    'df06a6ca-4345-46e7-957c-0905dc51cb8d' => OperationConfig.new("/mock_crud_server/users", "create", [], []),
+                    'f481fe9b-e21a-4ff5-8796-8fd5cad980fc' => OperationConfig.new("/mock_crud_server/users/{id}", "read", [], []),
+                    '79c5ec39-b816-421f-87da-a51fcbbe0ca0' => OperationConfig.new("/mock_crud_server/users/{id}", "update", [], []),
+                    'f3acd077-7a77-45db-90b2-d966bf408731' => OperationConfig.new("/mock_crud_server/users/{id}", "delete", [], []),
+                    'cb334878-e880-4940-ab10-29b89e171d90' => OperationConfig.new("/mock_crud_server/users200/{id}", "delete", [], []),
+                    '0049f449-4c7c-416e-aa5f-c3208ead83f6' => OperationConfig.new("/mock_crud_server/users204/{id}", "delete", [], []),
+                    
+                }
 
+                protected
 
-
-
-
-            def self.create(mapObj)
-                #
-                #Creates object of type User
-                #
-                #@param Dict mapObj, containing the required parameters to create a new object
-                #@return User of the response of created instance.
-                #
-                return self.execute("e53e61ff-ffa8-4d6f-9e2b-e85bfc70609a", User.new(mapObj))
-            end
-
-
-
-
-
-
-
-
-
-
-
-
-
-            def self.read(id, criteria = nil)
-                #
-                #Returns objects of type User by id and optional criteria
-                #@param str id
-                #@param dict criteria
-                #@return instance of User
-
-                mapObj = User.new
-                mapObj.set("id", id)
-                if !criteria.nil?
-                    mapObj.setAll(criteria)
+                def self.getOperationConfig(uuid)
+                    if @__store.key?(uuid)
+                        return @__store[uuid]
+                    end
+                    raise NotImplementedError.new("Invalid operationUUID supplied:"+ uuid)
                 end
 
-                return self.execute("989b48f8-87d2-4551-928e-441e3cc3a1ff",User.new(mapObj))
-            end
-
-
-
-
-            def update
-                #
-                #Updates an object of type User
-                #
-                #@return User object representing the response.
-                #
-                return self.class.execute("b39fbd58-cbf0-48ba-9971-e05101ec2309",self)
-            end
-
-
-
-
-
-
-
-
-
-
-            def self.deleteById(id, map = nil)
-                #Delete object of type User by id
-
-                #@param str id
-                #@param Dict map, containing additional parameters
-                #@return User of the response of the deleted instance.
-
-                mapObj = User.new
-                mapObj.set("id", id)
-                if !map.nil?
-                    mapObj.setAll(map)
+                def self.getOperationMetadata()
+                    return OperationMetadata.new("0.0.1", "http://localhost:8081")
                 end
 
-                return self.execute("54f33e70-0c93-472a-8369-b45c59ae11cb", mapObj)
-            end
+                public
 
 
-            def delete
-                #
-                #Delete object of type User
 
-                #@param str id
-                #@return User of the response of the deleted instance.
-                #
-                return self.class.execute("54f33e70-0c93-472a-8369-b45c59ae11cb", self)
-            end
+                def self.listByCriteria(criteria = nil)
+                    #
+                    #List objects of type User
+                    #
+                    #@param Dict criteria
+                    #@return Array of User object matching the criteria.
+
+                    if criteria.nil?
+                        return self.execute("6ac0b4ea-c45d-490b-96a5-840c4f8415e8",User.new)
+                    else
+                        return self.execute("6ac0b4ea-c45d-490b-96a5-840c4f8415e8",User.new(criteria))
+                    end
+                end
+
+
+
+                def self.create(mapObj)
+                    #
+                    #Creates object of type User
+                    #
+                    #@param Dict mapObj, containing the required parameters to create a new object
+                    #@return User of the response of created instance.
+                    #
+                    return self.execute("df06a6ca-4345-46e7-957c-0905dc51cb8d", User.new(mapObj))
+                end
+
+
+
+
+
+
+
+
+
+
+                def self.read(id, criteria = nil)
+                    #
+                    #Returns objects of type User by id and optional criteria
+                    #@param str id
+                    #@param dict criteria
+                    #@return instance of User
+
+                    mapObj = User.new
+                    if !(id.nil? || id.to_s.empty?)
+                      mapObj.set("id", id)
+                    end
+                    if !criteria.nil?
+                        if criteria.instance_of? RequestMap
+                          mapObj.setAll(criteria.getObject())
+                        else
+                          mapObj.setAll(criteria)
+                        end
+                    end
+
+                    return self.execute("f481fe9b-e21a-4ff5-8796-8fd5cad980fc",User.new(mapObj))
+                end
+
+
+
+                def update
+                    #
+                    #Updates an object of type User
+                    #
+                    #@return User object representing the response.
+                    #
+                    return self.class.execute("79c5ec39-b816-421f-87da-a51fcbbe0ca0",self)
+                end
+
+
+
+
+
+
+
+
+
+                def self.deleteById(id, map = nil)
+                    #Delete object of type User by id
+
+                    #@param str id
+                    #@param Dict map, containing additional parameters
+                    #@return User of the response of the deleted instance.
+
+
+                    mapObj = User.new
+                    if !(id.nil? || id.to_s.empty?)
+                      mapObj.set("id", id)
+                    end
+                    if !map.nil?
+                        if map.instance_of? RequestMap
+                          mapObj.setAll(map.getObject())
+                        else
+                          mapObj.setAll(map)
+                        end
+                    end
+
+                    return self.execute("f3acd077-7a77-45db-90b2-d966bf408731", mapObj)
+                end
+
+
+                def delete
+                    #
+                    #Delete object of type User
+
+                    #@param str id
+                    #@return User of the response of the deleted instance.
+                    #
+
+                    return self.class.execute("f3acd077-7a77-45db-90b2-d966bf408731", self)
+                end
+
+
+
+
+
+
+                def self.delete200ById(id, map = nil)
+                    #Delete object of type User by id
+
+                    #@param str id
+                    #@param Dict map, containing additional parameters
+                    #@return User of the response of the deleted instance.
+
+
+                    mapObj = User.new
+                    if !(id.nil? || id.to_s.empty?)
+                      mapObj.set("id", id)
+                    end
+                    if !map.nil?
+                        if map.instance_of? RequestMap
+                          mapObj.setAll(map.getObject())
+                        else
+                          mapObj.setAll(map)
+                        end
+                    end
+
+                    return self.execute("cb334878-e880-4940-ab10-29b89e171d90", mapObj)
+                end
+
+
+                def delete200
+                    #
+                    #Delete object of type User
+
+                    #@param str id
+                    #@return User of the response of the deleted instance.
+                    #
+
+                    return self.class.execute("cb334878-e880-4940-ab10-29b89e171d90", self)
+                end
+
+
+
+
+
+
+                def self.delete204ById(id, map = nil)
+                    #Delete object of type User by id
+
+                    #@param str id
+                    #@param Dict map, containing additional parameters
+                    #@return User of the response of the deleted instance.
+
+
+                    mapObj = User.new
+                    if !(id.nil? || id.to_s.empty?)
+                      mapObj.set("id", id)
+                    end
+                    if !map.nil?
+                        if map.instance_of? RequestMap
+                          mapObj.setAll(map.getObject())
+                        else
+                          mapObj.setAll(map)
+                        end
+                    end
+
+                    return self.execute("0049f449-4c7c-416e-aa5f-c3208ead83f6", mapObj)
+                end
+
+
+                def delete204
+                    #
+                    #Delete object of type User
+
+                    #@param str id
+                    #@return User of the response of the deleted instance.
+                    #
+
+                    return self.class.execute("0049f449-4c7c-416e-aa5f-c3208ead83f6", self)
+                end
+
         end
     end
 end
