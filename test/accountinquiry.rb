b50f7a080114abd4a0dafb2fdb4eca44df2c1dfa
Andrea Rizzini <andrea_rizzini@mastercard.com>
2016-11-28 14:42:53 +0000

    arizzini: adding account inquiry to test the payload oauth..

---
+++ test/accountinquiry.rb
@@ -0,0 +1,60 @@
+#
+# Copyright (c) 2016 MasterCard International Incorporated
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are
+# permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this list of
+# conditions and the following disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice, this list of
+# conditions and the following disclaimer in the documentation and/or other materials
+# provided with the distribution.
+# Neither the name of the MasterCard International Incorporated nor the names of its
+# contributors may be used to endorse or promote products derived from this software
+# without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+require "mastercard/core/model"
+
+module MasterCard
+  module Test
+    class AccountInquiry < MasterCard::Core::Model::BaseObject
+      include MasterCard::Core::Model
+
+      @__store = {
+        'update' => OperationConfig.new("/fraud/loststolen/v1/account-inquiry", "update", [], [])
+      }
+      
+      protected 
+      
+      def self.getOperationConfig(uuid)
+        if @__store.key?(uuid)
+          return @__store[uuid]
+        end
+        raise NotImplementedError.new("Invalid operationUUID supplied:"+ operationUUID)
+      end
+      
+      def self.getOperationMetadata()
+        return OperationMetadata.new("0.0.1", "https://sandbox.api.mastercard.com")
+      end
+
+      
+      public
+      
+      def self.update(criteria)
+        return self.execute("update", AccountInquiry.new(criteria))
+      end
+      
+    end
+  end
+end
---
+++ test/test_accountinquiry.rb
@@ -0,0 +1,64 @@
+#
+# Copyright (c) 2016 MasterCard International Incorporated
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are
+# permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this list of
+# conditions and the following disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice, this list of
+# conditions and the following disclaimer in the documentation and/or other materials
+# provided with the distribution.
+# Neither the name of the MasterCard International Incorporated nor the names of its
+# contributors may be used to endorse or promote products derived from this software
+# without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+require 'test_helper'
+require 'accountinquiry'
+require 'mastercard/security/oauth'
+require 'mastercard/core/model'
+
+
+class InsightsTest < Minitest::Test
+  include MasterCard::Security::OAuth
+  include MasterCard::Core
+  include MasterCard::Core::Model
+  include MasterCard::Test
+
+  def setup
+    keyFile =  File.join(File.dirname(__FILE__), "resources", "mcapi_sandbox_key.p12")
+    @auth = OAuth::OAuthAuthentication.new("L5BsiPgaF-O3qA36znUATgQXwJB6MRoMSdhjd7wt50c97279!50596e52466e3966546d434b7354584c4975693238513d3d",keyFile, "test", "password")
+    Config.setAuthentication(@auth)
+  end
+
+  def test_example_insights
+
+    mapObj = RequestMap.new
+
+    mapObj.set("Period","")
+    mapObj.set("CurrentRow","1")
+    mapObj.set("Sector","")
+    mapObj.set("Offset","25")
+    mapObj.set("Country","US")
+    mapObj.set("Ecomm","")
+
+
+    response = AccountInquiry.update(mapObj)
+
+    assert_equal("True",response.get("Account.Listed"))
+    assert_equal("S",response.get("Account.ReasonCode"))
+    assert_equal("STOLEN",response.get("Account.Reason"))
+
+  end
+end
